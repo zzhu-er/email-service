cad93e208d35a5937994bd65bea7a28356f9007e
zzhu-er <863955132@qq.com>
2023-03-10 11:40:41 +0800

    feat: use H2 database for testing

--- src/test/java/com/springhomework/emailAPI/EmailServiceTests.java
+++ src/test/java/com/springhomework/emailAPI/EmailServiceTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class EmailServiceTests {
     @MockBean
     private EmailRepository emailRepository;
